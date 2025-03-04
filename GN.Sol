547939244104c061e6cff48d98c78eac610b345f
Cyclique <jbllauro01@gmail.com>
2025-03-04 17:20:13 +0100

    Create GN.Sol

---
+++ GN.Sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+/**
+ * @title SayGN
+ * @dev Un contrat simple pour émettre un message "GN - Bonsoir !" via un événement.
+ */
+contract SayGN {
+    // Événement émis lorsqu'un utilisateur appelle la fonction sayGN
+    event GNSaid(address indexed user, string message);
+
+    /**
+     * @notice Émet l'événement GNSaid avec un message prédéfini.
+     */
+    function sayGN() external {
+        emit GNSaid(msg.sender, "GN - Good night !");
+    }
+}
